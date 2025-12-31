76945c71522b0b76391f4c098521d5a81e705d6b
MREZA <rezaworku31@gmail.com>
2026-01-01 01:34:16 +0700

    created : office space photos table migration

--- office-backend/database/migrations/2025_12_31_182140_create_office_space_photos_table.php
+++ office-backend/database/migrations/2025_12_31_182140_create_office_space_photos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('office_space_photos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('office_space_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
