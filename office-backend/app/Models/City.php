643905c720da10f0c423bddef852520765021db1
MREZA <rezaworku31@gmail.com>
2026-01-01 23:08:37 +0700

    created : relation for all models

--- office-backend/app/Models/City.php
+++ office-backend/app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
@@ -15,4 +16,8 @@ class City extends Model
         'photo',
         'slug',
     ];
+
+    public function officeSpaces():HasMany{
+        return $this->hasMany(OfficeSpace::class);
+    }
 }
