6f385869768ebc46773d5b60824120e05cdffcfc
MREZA <rezaworku31@gmail.com>
2026-01-05 13:15:45 +0700

    updated : installing api for laravel dan making router for every api

---
+++ office-backend/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\CityController;
+use App\Http\Controllers\Api\OfficeSpaceController;
+use App\Http\Resources\Api\BookingTransactionResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/city/{city:slug}', [CityController::class, 'show']);
+Route::apiResource('/cities', [CityController::class]);
+
+Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
+Route::apiResource('/cities', [OfficeSpaceController::class]);
+
+Route::post('/booking-transaction', [BookingTransactionResource::class, 'store']);
+Route::post('/checking-booking', [BookingTransactionResource::class],'booking_details');
