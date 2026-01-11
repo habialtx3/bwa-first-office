2683e208ab69625c36b7802a94a141ea27c2e9d7
MREZA <rezaworku31@gmail.com>
2026-01-11 22:43:22 +0700

    creatd : city details page

--- office-frontend/src/components/CityCard.tsx
+++ office-frontend/src/components/CityCard.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
 import type { City } from "../types/type"
 
 export default function CityCard({city} : CityCardProps) {
     const Base_Url = "http://127.0.0.1:8000/storage"
     return (
         <>
-            <div className="card">
+            <Link to={`city/${city.slug}`} className="card">
                 <div className="relative flex shrink-0 w-[230px] h-[300px] rounded-[20px] overflow-hidden">
                     <div className="relative flex flex-col justify-end w-full h-full p-5 gap-[2px] bg-[linear-gradient(180deg,_rgba(0,0,0,0)_49.87%,_rgba(0,0,0,0.8)_100%)] z-10">
                         <h3 className="font-bold text-xl leading-[30px] text-white">
@@ -18,7 +19,7 @@ export default function CityCard({city} : CityCardProps) {
                         alt="thumbnails"
                     />
                 </div>
-            </div>
+            </Link>
         </>
     )
 }
